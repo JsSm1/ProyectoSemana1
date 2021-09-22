68922e2fe86d83e902056ee45809a96ee186e524
JsSm1 <sebas.savedra@hotmail.com>
2021-09-21 21:33:45 -0500

    first comit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+alert("BIENVENIDO!!!");
+
+const album1 = () => {
+    albuncito1 = alert("Este es el album MECHANICAL RESONANCE ");
+    
+}
+
+const album2 = () => {
+     albuncito2 = alert("Este es el album PSYCHOTIC SUPER");
+    
+}
+
+const album3 = () => {
+     albuncito3 = alert("Este es el album BUST A NUT");
+    
+}
+
+const album4 = () => {
+     albuncito4 = alert("Este es el album FOREVER MORE ");
+    
+}
+
+const album5 = () => {
+      albuncito5 = alert("Este es el album SIMPLICITY ");
+    
+}
+
+const album6 = () => {
+     albuncito6 = alert("Este es el album SHOCK ");
+    
+}
